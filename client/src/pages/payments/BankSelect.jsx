3cba29f8abb4a730ccbb60d236e03bbaa5a58e96
hien-p <hphan0390@gmail.com>
2023-03-18 21:51:25 +0700

    bankselect bank css payment

--- client/src/pages/payments/BankSelect.jsx
+++ client/src/pages/payments/BankSelect.jsx
@@ -1,36 +1,139 @@
 import React, { useState } from 'react';
-// import './BankSelect.css';
+import './bank.css';
+import { useHistory } from 'react-router-dom';
+import {Link} from 'react-router-dom'
+
+const banks = [
+  {
+    name: 'Vietcombank',
+    logo: 'https://hopdungcardvisit.com/wp-content/uploads/2019/05/logo-ngan-hang-Vietcombank.png',
+    value: 'vietcombank',
+    accountInfo: {
+    label: 'Account number',
+    placeholder: 'Enter your Vietcombank account number',
+    pattern: '[0-9]{10}',
+    errorMessage: 'Please enter a valid 10-digit Vietcombank account number',
+},
+  },
+  {
+    name: 'Techcombank',
+    logo: 'https://hopdungcardvisit.com/wp-content/uploads/2019/05/logo-ngan-hang-Techcombank.png',
+    value: 'techcombank',
+    accountInfo: {
+    label: 'Account number',
+    placeholder: 'Enter your Techcombank account number',
+    pattern: '[0-9]{9}',
+    errorMessage: 'Please enter a valid 9-digit Techcombank account number',
+    },},
+    {
+        name: 'Agribank',
+        logo: 'https://hopdungcardvisit.com/wp-content/uploads/2019/05/Agribank-logo.png',
+        value: 'Agribank',
+        accountInfo: {
+        label: 'Account number',
+        placeholder: 'Enter your Agribank account number',
+        pattern: '[0-9]{9}',
+        errorMessage: 'Please enter a valid 9-digit Agribank account number',
+        },
+    },
+    {
+    name: 'Vietinbank',
+    logo: 'https://hopdungcardvisit.com/wp-content/uploads/2019/05/logo-ngan-hang-Vietinbank.png',
+    value: 'Vietinbank',
+    accountInfo: {
+        label: 'Account number',
+        placeholder: 'Enter your Vietinbank account number',
+        pattern: '[0-9]{9}',
+        errorMessage: 'Please enter a valid 9-digit Vietinbank account number',
+    },
+    },{
+        name: 'Mbbank ',
+        logo: 'https://hopdungcardvisit.com/wp-content/uploads/2019/05/logo-ngan-hang-MB.png',
+        value: 'Mbbank ',
+        accountInfo: {
+            label: 'Account number',
+            placeholder: 'Enter your Mbbank  account number',
+            pattern: '[0-9]{9}',
+            errorMessage: 'Please enter a valid 9-digit Mbbank  account number',
+        },
+        },{
+            name: 'Bidv',
+            logo: 'https://hopdungcardvisit.com/wp-content/uploads/2019/05/logo-ngan-hang-BIDV.png',
+            value: 'Bidv',
+            accountInfo: {
+                label: 'Account number',
+                placeholder: 'Enter your Bidv account number',
+                pattern: '[0-9]{9}',
+                errorMessage: 'Please enter a valid 9-digit Bidv account number',
+            },
+            }
+
+];
+
 
 function BankSelect() {
-const [selectedBank, setSelectedBank] = useState('');
+    const [selectedBank, setSelectedBank] = useState(banks[0].value);
+  const [accountNumber, setAccountNumber] = useState('');
 
-const handleBankSelect = (event) => {
-setSelectedBank(event.target.value);
-};
+  const handleBankSelect = (bankValue) => {
+    setSelectedBank(bankValue);
+    setAccountNumber('');
+  };
 
-const handleSubmit = (event) => {
-event.preventDefault();
-// perform action based on selected bank
-};
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(`Submitting account number ${accountNumber} for bank ${selectedBank}`);
+  };
 
-return (
-<form className="bank-select" onSubmit={handleSubmit}>
-<label htmlFor="bank-select">Chọn ngân hàng:</label>
-<select id="bank-select" name="bank" value={selectedBank} onChange={handleBankSelect}>
-<option value="">-- Vui lòng chọn ngân hàng --</option>
-<option value="vietcombank">Vietcombank</option>
-<option value="techcombank">Techcombank</option>
-<option value="vnpay">VNPay</option>
-<option value="sacombank">Sacombank</option>
-</select>
-<button type="submit">Chuyển đến</
+  const selectedBankObj = banks.find((bank) => bank.value === selectedBank);
 
-
-
-
-++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++button>
-</form>
-);
+  return (
+    <div>
+      <div className="bank-select-container">
+        {banks.map((bank) => (
+          <div
+            key={bank.value}
+            className={`bank-card ${selectedBank === bank.value ? 'selected' : ''}`}
+            onClick={() => handleBankSelect(bank.value)}
+          >
+            <div className="bank-logo">
+              <img src={bank.logo} alt={bank.name} />
+            </div>
+            <div className="bank-name">
+              {bank.name}
+            </div>
+          </div>
+        ))}
+      </div>
+      {selectedBankObj && (
+        <form onSubmit={handleSubmit}>
+          <h2>Enter your account number for {selectedBankObj.name}:</h2>
+          <label htmlFor="accountNumber">{selectedBankObj.accountInfo.label}</label>
+          <input
+            type="text"
+            id="accountNumber"
+            placeholder={selectedBankObj.accountInfo.placeholder}
+            pattern={selectedBankObj.accountInfo.pattern}
+            value={accountNumber}
+            onChange={(event) => setAccountNumber(event.target.value)}
+            required
+          />
+          <span className="error-message">{selectedBankObj.accountInfo.errorMessage}</span>
+            <Link to='/payment'> <button type="submit">Submit</button> </Link>
+            
+        </form>
+      )}
+    </div>
+  );
 }
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+    history.push(`/payment?bank=${selectedBankObj.value}&accountNumber=${accountNumber}`);
+  }
+  
+  const history = useHistory();
+
 
 export default BankSelect;
--- client/src/pages/payments/Payment.jsx
+++ client/src/pages/payments/Payment.jsx
@@ -1,104 +1,123 @@
 
-import './payment.css';
+// import './payment.css';
 
-import React, { useState }  from 'react';
+// import React, { useState }  from 'react';
+
+// function Payment() {
+//   const [name, setName] = useState('');
+//   const [email, setEmail] = useState('');
+//   const [cardNumber, setCardNumber] = useState('');
+//   const [expiryMonth, setExpiryMonth] = useState('');
+//   const [expiryYear, setExpiryYear] = useState('');
+//   const [cvv, setCvv] = useState('');
+
+//   const handleSubmit = (e) => {
+//     e.preventDefault();
+//     // Validate form inputs
+//     // Process payment
+//     // Display success/failure message
+//   };
+
+//   return (
+//     <div className="container">
+//       <h2>Make a Payment</h2>
+//       <form onSubmit={handleSubmit} className="form">
+//         <div className="form-group">
+//           <label htmlFor="name">Name on Card:</label>
+//           <input
+//             type="text"
+//             id="name"
+//             value={name}
+//             onChange={(e) => setName(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="email">Email:</label>
+//           <input
+//             type="email"
+//             id="email"
+//             value={email}
+//             onChange={(e) => setEmail(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="cardNumber">Card Number:</label>
+//           <input
+//             type="text"
+//             id="cardNumber"
+//             value={cardNumber}
+//             onChange={(e) => setCardNumber(e.target.value)}
+//             required
+//           />
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="expiryMonth">Expiration Date:</label>
+//           <div className="expiry-date">
+//             <input
+//               type="number"
+//               id="expiryMonth"
+//               value={expiryMonth}
+//               onChange={(e) => setExpiryMonth(e.target.value)}
+//               placeholder="MM"
+//               min="1"
+//               max="12"
+//               required
+//             />
+//             <span>/</span>
+//             <input
+//               type="number"
+//               id="expiryYear"
+//               value={expiryYear}
+//               onChange={(e) => setExpiryYear(e.target.value)}
+//               placeholder="YY"
+//               min={new Date().getFullYear().toString().substr(-2)}
+//               max={(new Date().getFullYear() + 10).toString().substr(-2)}
+//               required
+//             />
+//           </div>
+//         </div>
+//         <div className="form-group">
+//           <label htmlFor="cvv">CVV:</label>
+//           <input
+//             type="number"
+//             id="cvv"
+//             value={cvv}
+//             onChange={(e) => setCvv(e.target.value)}
+//             placeholder="123"
+//             minLength="3"
+//             maxLength="4"
+//             required
+//           />
+//         </div>
+//         <button className="pay-button">Pay Now</button>
+//       </form>
+//     </div>
+//   );
+// }
+
+// export default Payment;
+
+
+import { useLocation } from 'react-router-dom';
 
 function Payment() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [cardNumber, setCardNumber] = useState('');
-  const [expiryMonth, setExpiryMonth] = useState('');
-  const [expiryYear, setExpiryYear] = useState('');
-  const [cvv, setCvv] = useState('');
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const bank = params.get('bank');
+  const accountNumber = params.get('accountNumber');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Validate form inputs
-    // Process payment
-    // Display success/failure message
-  };
-
+  // use bank and accountNumber to display payment information
   return (
-    <div className="container">
-      <h2>Make a Payment</h2>
-      <form onSubmit={handleSubmit} className="form">
-        <div className="form-group">
-          <label htmlFor="name">Name on Card:</label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="cardNumber">Card Number:</label>
-          <input
-            type="text"
-            id="cardNumber"
-            value={cardNumber}
-            onChange={(e) => setCardNumber(e.target.value)}
-            required
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="expiryMonth">Expiration Date:</label>
-          <div className="expiry-date">
-            <input
-              type="number"
-              id="expiryMonth"
-              value={expiryMonth}
-              onChange={(e) => setExpiryMonth(e.target.value)}
-              placeholder="MM"
-              min="1"
-              max="12"
-              required
-            />
-            <span>/</span>
-            <input
-              type="number"
-              id="expiryYear"
-              value={expiryYear}
-              onChange={(e) => setExpiryYear(e.target.value)}
-              placeholder="YY"
-              min={new Date().getFullYear().toString().substr(-2)}
-              max={(new Date().getFullYear() + 10).toString().substr(-2)}
-              required
-            />
-          </div>
-        </div>
-        <div className="form-group">
-          <label htmlFor="cvv">CVV:</label>
-          <input
-            type="number"
-            id="cvv"
-            value={cvv}
-            onChange={(e) => setCvv(e.target.value)}
-            placeholder="123"
-            minLength="3"
-            maxLength="4"
-            required
-          />
-        </div>
-        <button className="pay-button">Pay Now</button>
-      </form>
+    <div>
+      <h2>Payment Information:</h2>
+      <p>Bank: {bank}</p>
+      <p>Account Number: {accountNumber}</p>
+      // other payment information
     </div>
   );
 }
 
 export default Payment;
-
-
-
 
